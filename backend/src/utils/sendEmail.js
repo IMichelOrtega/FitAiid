6df0a095a20d1c4c8ed8fd49cc4bd2afb51aea6b
Michel Ortega <michelsortega444@gmail.com>
2026-03-05 17:01:59 -0500

    fix agg EMAIL_SECURE=TRUE

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -10,7 +10,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || "smtp.gmail.com",
   port: parseInt(process.env.EMAIL_PORT || "587"),
-  secure: false, // true para 465, false para otros
+  secure: process.env.EMAIL_SECURE === "true", // true para 465, false para otros
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
